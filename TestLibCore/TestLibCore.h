8a30b020dd1afcdc73b41d2afe5a23c607426ef5
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2019-12-25 16:40:46 +0700

    Created 2 frameworks
    first one is TestLibCore going to be the framework that everyone call it
    second one is TestLibTest is Test helper is everyone what to use core so no need for duplicated code

---
+++ TestLibCore/TestLibCore.h
@@ -0,0 +1,19 @@
+//
+//  TestLibCore.h
+//  TestLibCore
+//
+//  Created by Wasith Theerapattrathamrong on 25/12/19.
+//  Copyright © 2019 Ayudhya Capital Auto Lease Plc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibCore.
+FOUNDATION_EXPORT double TestLibCoreVersionNumber;
+
+//! Project version string for TestLibCore.
+FOUNDATION_EXPORT const unsigned char TestLibCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibCore/PublicHeader.h>
+
+
---
+++ TestLibTest/TestLibTest.h
@@ -0,0 +1,19 @@
+//
+//  TestLibTest.h
+//  TestLibTest
+//
+//  Created by Wasith Theerapattrathamrong on 25/12/19.
+//  Copyright © 2019 Ayudhya Capital Auto Lease Plc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibTest.
+FOUNDATION_EXPORT double TestLibTestVersionNumber;
+
+//! Project version string for TestLibTest.
+FOUNDATION_EXPORT const unsigned char TestLibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibTest/PublicHeader.h>
+
+
