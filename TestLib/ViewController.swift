c197ebc82392fb09969fa5363bd8c3f70a155003
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2019-12-25 17:35:22 +0700

    rearrange

--- TestLib/ViewController.swift
+++ TestLib/ViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
+import TestLibCore
 
 class ViewController: UIViewController {
 
-}
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        Tracker.standard.send(event: Self.identifier)
+    }
+}
