9487515068c23ebcd047f67d452aee08732ce21d
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2019-12-25 16:58:50 +0700

    Dummy Tracker with tests

---
+++ TestLibCore/Tracker.swift
@@ -0,0 +1,9 @@
+//
+//  Tracker.swift
+//  TestLibCore
+//
+//  Created by Wasith Theerapattrathamrong on 25/12/19.
+//  Copyright © 2019 Ayudhya Capital Auto Lease Plc. All rights reserved.
+//
+
+import Foundation
